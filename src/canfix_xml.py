38501425f4ba3da2cef3656cb5b0f2c8663d8592
Phil Birkelbach <phil@petrasoft.net>
2018-05-04 10:32:14 -0500

    Switch canfix_xml.py from xlm parsing the spreadsheet to pyexcel.

--- src/canfix_xml.py
+++ src/canfix_xml.py
@@ -2,63 +2,16 @@
 
 # This program parses the Spreadsheet that has all of our CANFix definitions and
 # creates the official canfix.xml file
- 
-# import the needed modules
-import zipfile
-import xml.parsers.expat
 
-inputfile = "CAN-FIX-0.6.ods"
+#
+import pyexcel as pe
+
+inputfile = "CAN-FIX.ods"
 outputfile = "canfix.xml"
-canfixVersion = "0.6"
+canfixVersion = "0.7"
 
-class TableBuilder:
-    def __init__(self):
-        self.tables = {}
-        self.row = 0
-        self.cell = None
-        self.table = []
-        self.row = []
-    def start_element(self, name, attrs):
-        if name == "table:table":
-          print "Parsing Table: " + attrs["table:name"]
-          self.tables[attrs["table:name"]] = list()
-          self.table = self.tables[attrs["table:name"]]
-        if name == "table:table-row":
-          self.row = []
-        if name == "table:table-cell":
-          if "table:number-columns-repeated" in attrs.keys():
-            for i in range(int(attrs["table:number-columns-repeated"]) -1):
-              self.row.append(None)
-          self.cell = None
-    def end_element(self, name):
-        if name == "table:table-row":
-          self.table.append(self.row)
-        if name == "table:table-cell":
-          self.row.append(self.cell)
-    def char_data(self, data):
-        self.cell = data
-        
-# get content xml data from OpenDocument file
-ziparchive = zipfile.ZipFile(inputfile, "r")
-xmldata = ziparchive.read("content.xml")
-ziparchive.close()
-print xmldata
+tablebuilder = pe.get_book(file_name=inputfile)
 out = open(outputfile, 'w')
-
-# create parser and parsehandler
-parser = xml.parsers.expat.ParserCreate()
-tablebuilder = TableBuilder()
-# assign the handler functions
-parser.StartElementHandler  = tablebuilder.start_element
-parser.EndElementHandler    = tablebuilder.end_element
-parser.CharacterDataHandler = tablebuilder.char_data
-parser.DefaultHandler       = tablebuilder.char_data
- 
-# parse the data
-parser.Parse(xmldata, True)
-
-# At this point tablebuilder.tables is a dictionary of lists of lists.  The dictionary
-# represents the sheet name and the list of lists is the table
 
 NameColumn = 0
 StartColumn = 3
@@ -67,8 +20,10 @@ EndColumn = 6
 out.write("<protocol>\n<name>CANFIX</name>\n")
 out.write("<version>%s</version>\n" % canfixVersion)
 
-for row in tablebuilder.tables["Groups"][1:]:
-    if row[NameColumn] != None:
+
+for x, row in enumerate(tablebuilder["Groups"]):
+    if x < 1: continue  # Not sure why slicing doesn't work on sheets - quick hack
+    if row[NameColumn]:
         out.write("<group>\n")
         out.write("  <name>%s</name>\n" % row[NameColumn])
         out.write("  <startid>%s</startid>\n" % row[StartColumn])
@@ -87,21 +42,22 @@ indexColumn = 9
 remarksColumn = 10
 auxColumnStart = 11
 
-for row in tablebuilder.tables["Identifiers"][2:]:
-    if row[idColumn] != None:
+for x,row in enumerate(tablebuilder["Identifiers"]):
+    if x < 2: continue
+    if row[idColumn]:
         print "Writing Parameter: %s" % row[nameColumn]
         out.write("<parameter>\n")
         out.write("  <id>%s</id>\n" % row[idColumn])
         out.write("  <count>%s</count>\n" % row[countColumn])
         out.write("  <name>%s</name>\n" % row[nameColumn])
-        
+
         if row[typeColumn] == None:
             print("Warning: Type not set for %s" % row[nameColumn])
             s = ""
         else:
             s = row[typeColumn]
         out.write("  <type>%s</type>\n" % s)
-                
+
         if row[rangeColumn] != None:
             s = row[rangeColumn]
             if s.find(" to ") != -1:
@@ -112,13 +68,14 @@ for row in tablebuilder.tables["Identifiers"][2:]:
                 #out.write("  </range>\n")
             else:
                 out.write("  <format>%s</format>\n" % s)
-        
+
         if row[offsetColumn] != None:
             out.write("  <offset>%s</offset>\n" % row[offsetColumn])
-        
+
         if row[unitsColumn] != None:
+            i = 0
             s = row[unitsColumn].replace(u'\xba',u'deg')
-            for i in range(len(s)-1, -1, -1):
+            for i in range(len(s)-1):
                 if s[i].isdigit(): break
             if i == 0:
                 mult = None
@@ -127,11 +84,12 @@ for row in tablebuilder.tables["Identifiers"][2:]:
                 i=i+1
                 mult = s[:i]
                 units = s[i:].strip()
-                
+
             if mult:
                 out.write("  <multiplier>%s</multiplier>\n" % mult)
-            out.write("  <units>%s</units>\n" % units)
-        
+            s = u"  <units>%s</units>\n" % units
+            out.write(s.encode("UTF-8"))
+
         if row[indexColumn] != None:
             out.write("  <index>%s</index>\n" % row[indexColumn])
         if row[remarksColumn] != None:
@@ -144,9 +102,9 @@ for row in tablebuilder.tables["Identifiers"][2:]:
             s = row[auxColumnStart + i]
             if s != None:
                 out.write("  <aux id='%d'>%s</aux>\n" % (i+1, s))
-       
+
         out.write("</parameter>\n")
-    
+
 out.write("</protocol>\n")
-       
+
 out.close
